7de5175ae984e088909effbc37df59867dcd393a
Jaedon <Jsatchell2017@gmail.com>
2024-04-19 11:14:04 +0200

    Finished simple version of app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ec57d5720emshc807075ae0f2c25p1579a4jsnaaf2e182320a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/"
+export const WEATHER_API_KEY =  "53c84f712ee1579346de5b4b78306b5e";
